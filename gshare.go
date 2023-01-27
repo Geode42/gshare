d7b263042f0887de68e156a934df6b4045ffe302
geode42 <99060684+geode42@users.noreply.github.com>
2023-01-27 18:10:41 -0500

    Add files via upload

--- gshare.go
+++ gshare.go
@@ -20,6 +20,12 @@ const (
 	CHUNKSIZE = 1024
 )
 
+func checkerr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func fileExists(path string) (bool) {
 	if _, err := os.Stat(path); err == nil {
 		return true
@@ -33,6 +39,17 @@ func fileExists(path string) (bool) {
 	}
 }
 
+func InfoPrint(info ...any) {
+	fmt.Print("\033[2m")
+	for i, word := range info {
+		fmt.Printf("%v", word)
+		if i != len(info) - 1 {
+			fmt.Print(" ")
+		}
+	}
+	fmt.Println("\033[0m")
+}
+
 // gshare <address> [file]
 
 func sendFile(ipAddress, filePath string) {
@@ -40,53 +57,69 @@ func sendFile(ipAddress, filePath string) {
 
 	// Listen for connections
 	ln, err := net.Listen("tcp", ":" + PORT)
-	if err != nil {
-		panic(err)
-	}
+	InfoPrint("Server hosted on port", PORT)
+	checkerr(err)
 
 	var conn net.Conn
 	for {
-		// Accept the incoming connection
+		// Accept any incominging connections
 		conn, err = ln.Accept()
-		if err != nil {
-			panic(err)
-		}
-		// Close it when done
-		defer conn.Close()
+		checkerr(err)
+		
+		
 		// Get the remote address
 		remoteAddress, _, _ := strings.Cut(conn.RemoteAddr().String(), ":")
-		// Look for new connections if this one isn't whitelisted
+		// Close the connection and redo the loop
 		if remoteAddress != ipAddress {
-			fmt.Println("Not whitelisted connection from", remoteAddress)
+			conn.Close()
+			InfoPrint("Rejected connection from", remoteAddress)
 			continue
 		}
+
+		// Close the connection when done
+		defer conn.Close()
+
 		// Break out of the loop
-		fmt.Println("New connection from", remoteAddress)
+		InfoPrint("Connection established with", remoteAddress)
 		break
 	}
 	// Open file for reading
-	f, err := os.Open(filePath)
-	if err != nil {
-		panic(err)
-	}
+	file, err := os.Open(filePath)
+	checkerr(err)
 	// Close it when done
-	defer f.Close()
+	defer file.Close()
 
 	// ---------- Send Filename --------------------
 
-	// Get filename
-	filename := f.Name() // returns just the filename and not the path
-	// fmt.Printf("%X\n", filename) // DEBUGGING
-	filenameBytes := []byte(filename)
 	// Send the filename
-	conn.Write(filenameBytes)
-	// fmt.Printf("%X", filename) // DEBUGGING
+	// TODO: UDPATE
+	a := []byte(file.Name())
+	fmt.Println(a)
+	fmt.Println(len(a))
+	conn.Write(a) // f.Name gets just the filename, without the full path
+	InfoPrint("Filename sent")
+
+
+
+	// // ---------- Create Info Bytes --------------------
+	// info := make([]byte, 4)
+
+	// ---------- Send Permissions --------------------
 	// Get permissions
 	info, _ := os.Stat(filePath)
 	perm := uint32(info.Mode())
+	fmt.Println("mode", info.Mode())
 	permByte := make([]byte, 4)
 	binary.BigEndian.PutUint32(permByte, perm)
 	conn.Write(permByte)
+	InfoPrint("Permissions sent")
+
+	// ---------- Send Chunk Count --------------------
+	chunkCount := (info.Size() + CHUNKSIZE - 1) / CHUNKSIZE // Divide by chunksize, round up
+	chunkCountByte := make([]byte, 8)
+	binary.BigEndian.PutUint64(chunkCountByte, uint64(chunkCount))
+	conn.Write(chunkCountByte)
+	InfoPrint("Chunk count sent")
 	
 	// ---------- Send File in Chunks --------------------
 
@@ -94,46 +127,66 @@ func sendFile(ipAddress, filePath string) {
 	// Create read buffer
 	readBuffer := make([]byte, CHUNKSIZE)
 	// Create reader
-	r := io.Reader(f)
+	reader := io.Reader(file)
 
 
 	for {
 		// Read next chunk
-		n, err := r.Read(readBuffer)
+		n, err := reader.Read(readBuffer)
 		if err != nil {
 			break
 		}
+		// Send chunk
 		conn.Write(readBuffer[:n])
-		// fmt.Println("hi")
 	}
+
+	InfoPrint(chunkCount, "chunks sent")
 }
 
 func receiveFile(ipAddress string) {
 	// ---------- Get Socket Connection --------------------
 	// Connect to IP address through port
 	conn, err := net.Dial("tcp", ipAddress + ":" + PORT)
-	if err != nil {
-		panic(err)
-	}
+	// fmt.Println(errors.Unwrap(err).Error())
+	// errors.As(err, errors.Unwrap(err))
+	// fmt.Println(err.Error() == "connect: no route to host")
+	// if err.Error() == "no route to host" {
+	// 	fmt.Println("hi")
+	// 	return
+	// }
+	// var a *net.Error
+	// if errors.As(err, &a) {
+	// 	fmt.Println("hi", a)
+	// }
+	checkerr(err)
 	// Close the connection when done
 	defer conn.Close()
 
 
 	// Get filename
 	filenameBuffer := make([]byte, 1024)
-	// fmt.Println(filenameBuffer) // DEBUG
 	n, err := conn.Read(filenameBuffer)
-	// fmt.Println(n) // DEBUG
-	if err != nil {
-		panic(err)
+	fmt.Println(filenameBuffer)
+	fmt.Println(len(filenameBuffer))
+	// The if the server closed the connection, conn.Read above will put an io.EOF into err
+	if errors.Is(err, io.EOF) {
+		InfoPrint("Server rejected connection; perhaps they mistyped your address")
+		return
 	}
-	filename := strings.TrimSpace(string(filenameBuffer)[:n])
+	checkerr(err)
+	filename := strings.TrimSpace(string(filenameBuffer[:n]))
 	// fmt.Printf("%X\n", filename) // DEBUG
 
 	// Get permissions
-	permBuffer := make([]byte, 1024)
+	permBuffer := make([]byte, 4)
 	conn.Read(permBuffer)
 	perm := os.FileMode(binary.BigEndian.Uint32(permBuffer))
+
+	// Get chunk count
+	chunkCountBuffer := make([]byte, 8)
+	conn.Read(chunkCountBuffer)
+	chunkCount := int64(binary.BigEndian.Uint64(chunkCountBuffer))
+	fmt.Println(chunkCount)
 
 	// Print info
 	fmt.Println("Getting the file \"" + filename + "\"")
@@ -181,7 +234,7 @@ func receiveFile(ipAddress string) {
 
 	dataBuffer := make([]byte, CHUNKSIZE)
 
-	for {
+	for i := int64(0); i < chunkCount; i++ {
 		n, err := conn.Read(dataBuffer)
 		// fmt.Println(n) // DEBUG
 		if err != nil {
